4c9fa77c0bfc2b0bdc9e84411ec131e7fe51caef
yarzou <yarzou@gmail.com>
2018-06-22 17:09:46 +0200

    update j-2

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ let tour = 0;
 
 //Fonction Match Nul
 let matchNul = document.createElement('div');
+matchNul.style.zIndex = '+1';
 matchNul.style.position = 'absolute';
 matchNul.style.textAlign = 'center';
+matchNul.style.width = '200px';
+matchNul.style.height = '125px';
 matchNul.style.fontSize = '20';
+matchNul.innerText = 'MATCH NUL !';
 function fMatchNul() {
     if (tour==9) {
         console.log('Match Nul !');
         event.target.appendChild(matchNul);
-    }
-}
+    };
+};
 
 table.addEventListener('click', function(event) {
     let cercle = document.createElement('div');
@@ -27,7 +31,10 @@ table.addEventListener('click', function(event) {
     cercle.style.borderRadius = '50%';
 
     let croix = document.createElement('h1');
-    croix.innerText = 'X';
+    croix.style.height = '70px';
+    croix.style.width = '70px';
+    croix.style.margin = 'auto';
+    croix.innerText = '<h1>X</h1>';
     croix.style.textAlign = 'center';
     
     
@@ -52,3 +59,13 @@ table.addEventListener('click', function(event) {
     fMatchNul();
 });
 
+
+
+
+// document.querySelectorAll('table morpion td').forEach((cell, index, list) =>{
+//     console.log(cell, index, list)
+// });
+// --> va chercher tous les td de la table de classe 'morion'.
+// 
+// classlist -> donne un tableau
+// classList[0].split('-')[1] -> donne l'indice de la case
